7421f2343fb37367ce9d09317c41ddea798235a1
Enrique Pereira <enrique.pereira.ssp@gmail.com>
2013-09-23 18:33:50 -0300

    Corrige comportamento do placeholder para IE

--- foundation-ui/src/main/java/com/brazoft/foundation/gwt/client/ui/binding/PlaceholderHTML4.java
+++ foundation-ui/src/main/java/com/brazoft/foundation/gwt/client/ui/binding/PlaceholderHTML4.java
@@ -3,64 +3,83 @@ package com.brazoft.foundation.gwt.client.ui.binding;
 import com.brazoft.foundation.gwt.client.ui.api.Input;
 import com.google.gwt.dom.client.InputElement;
 import com.google.gwt.event.dom.client.*;
+import com.google.gwt.user.client.Window;
 
 public class PlaceholderHTML4
     implements Placeholder {
 
-	private String lastValue   = "";
-	private String placeholder = "";
+        private String lastValue   = "";
+        private String placeholder = "";
 
-	@Override
-	public void apply(final Input<?, String> input, String placeholder) {
-		this.placeholder = placeholder;
+        @Override
+        public void apply(final Input<?, String> input, String placeholder) {
+                this.placeholder = placeholder;
 
-		// Coloca texto padrão
-		input.value(placeholder);
-		input.onFocus(new FocusHandler() {
+                // Coloca texto padrão
+                input.value(placeholder);
+                input.onFocus(new FocusHandler() {
 
-			@Override
-			public void onFocus(FocusEvent event) {
-				if (input.getValue().equals(PlaceholderHTML4.this.placeholder)) {
-					input.cursorAt(0);
-				}
-			}
-		});
-		input.onBlur(new BlurHandler() {
+                        @Override
+                        public void onFocus(FocusEvent event) {
+                                if (input.getValue().equals(PlaceholderHTML4.this.placeholder)) {
+                                        input.cursorAt(0);
+                                }
+                        }
+                });
+                input.onBlur(new BlurHandler() {
+                        @Override
+                        public void onBlur(BlurEvent event) {
+                                if (input.getValue().isEmpty()) {
+                                        input.value(PlaceholderHTML4.this.placeholder);
+                                }
+                        }
+                });
+                input.onKeyDown(new KeyDownHandler() {
+                        @Override
+                        public void onKeyDown(KeyDownEvent event) {
+                                // Ignora algumas teclas para evitar efeito desagradável
+                                if (event.getNativeKeyCode() != KeyCodes.KEY_ALT
+                                        && event.getNativeKeyCode() != KeyCodes.KEY_CTRL
+                                        && event.getNativeKeyCode() != KeyCodes.KEY_SHIFT
+                                        && event.getNativeKeyCode() != KeyCodes.KEY_PAGEUP
+                                        && event.getNativeKeyCode() != KeyCodes.KEY_PAGEDOWN) {
+                                    if (input.getValue().isEmpty() || input.getValue().equals(PlaceholderHTML4.this.placeholder)) {
+                                        input.value("");
+                                    }
+                                }
+                        }
+                });
+                input.onKeyUp(new KeyUpHandler() {
+                    @Override
+                    public void onKeyUp(KeyUpEvent event) {
+                        if (input.getValue().isEmpty()) {
+                            input.value(PlaceholderHTML4.this.placeholder);
+                            input.cursorAt(0);
+                        }
+                    }
+                });
+                input.onClick(new ClickHandler() {
 
-			@Override
-			public void onBlur(BlurEvent event) {
-				String value = input.getValue();
-				if (!PlaceholderHTML4.this.lastValue.equals(value)) {
-					PlaceholderHTML4.this.lastValue = value;
-				}
+                    @Override
+                    public void onClick(ClickEvent event) {
+                        // Se o texto do placeholder, selecione o texto todo
+                        if (input.getValue().equals(PlaceholderHTML4.this.placeholder)) {
+                            input.cursorAt(0);
+                        } else {
+                            input.selectAll();
+                        }
+                    }
+                });
+        }
 
-				if (value.isEmpty()) {
-					input.value(PlaceholderHTML4.this.placeholder);
-				}
-			}
-		});
-		input.onChange(new ChangeHandler() {
-
-			@Override
-			public void onChange(ChangeEvent event) {
-				if (input.getValue().equals(PlaceholderHTML4.this.placeholder)) {
-					input.addStyleName("placeholder");
-					return;
-				}
-				
-				input.removeStyleName("placeholder");
-			}
-		});
-	}
-	
-	@Override
-	public String getValue(Input<?, String> input) {
-	    String value = ((InputElement) input.getElement().cast()).getValue();
-	    
-	    if(this.placeholder.equals(value)){
-	    	value = "";
-	    }
-	    
-		return value;
-	}
+        @Override
+        public String getValue(Input<?, String> input) {
+            String value = ((InputElement) input.getElement().cast()).getValue();
+            
+            if(this.placeholder.equals(value)){
+                value = "";
+            }
+            
+                return value;
+        }
 }
